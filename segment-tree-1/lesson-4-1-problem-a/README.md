a572abfa1d80c9c983010c9e4f320b350d5e8785
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-04-27 11:13:41 +0300

    A. Дерево отрезков на сумму

---
+++ segment-tree-1/lesson-4-1-problem-a/README.md
@@ -0,0 +1,3 @@
+# A. Дерево отрезков на сумму
+
+https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/A
---
+++ segment-tree-1/lesson-4-1-problem-a/main.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define F first
+#define S second
+#define PB push_back
+
+template<typename T>
+class SegTree { // Дерево отрезков на сумму
+ public:
+  explicit SegTree(const std::vector<T>& a) {
+    while (size_ < CI(a.size())) size_ *= 2;
+    t_.resize(size_ * 2 - 1);
+    Init(a, 0, 0, size_);
+  }
+
+  T Sum(int l, int r) {
+    return Sum(l, r, 0, 0, size_);
+  }
+
+  void Set(int i, T v) {
+    Set(i, v, 0, 0, size_);
+  }
+
+ private:
+  std::vector<T> t_;
+  int size_ = 1;
+
+  void Init(const std::vector<T>& a, int x, int lx, int rx) {
+    if (lx + 1 == rx) {
+      if (lx < CI(a.size())) t_[x] = a[lx];
+    } else {
+      const auto m = (lx + rx) / 2;
+      Init(a, 2*x+1, lx, m);
+      Init(a, 2*x+2, m, rx);
+      t_[x] = t_[2*x+1] + t_[2*x+2];
+    }
+  }
+
+  T Sum(int l, int r, int x, int lx, int rx) {
+    if (rx <= l || lx >= r) return 0;
+    if (rx <= r && lx >= l) return t_[x];
+    const auto m = (lx + rx) / 2;
+    return Sum(l, r, 2*x+1, lx, m) + Sum(l, r, 2*x+2, m, rx);
+  }
+
+  void Set(int i, T v, int x, int lx, int rx) {
+    if (lx + 1 == rx) {
+      t_[x] = v;
+      return;
+    }
+    const auto m = (lx + rx) / 2;
+    if (i < m) Set(i, v, 2*x+1, lx, m);
+    else Set(i, v, 2*x+2, m, rx);
+    t_[x] = t_[2*x+1] + t_[2*x+2];
+  }
+};
+
+void Solution(std::istream& cin, std::ostream& cout) {
+  ll n, m;
+  cin >> n >> m;
+  vl a(n);
+  FOR(i, 0, n-1) {
+    cin >> a[i];
+  }
+  SegTree tree(a);
+  FOR(j, 0, m-1) {
+    int op; cin >> op;
+    if (op == 1) {
+      int i; ll v; cin >> i >> v;
+      tree.Set(i, v);
+    } else {
+      int l, r; cin >> l >> r;
+      cout << tree.Sum(l, r) << '\n';
+    }
+  }
+}
+
+[[maybe_unused]] void TestSolution() {
+  std::istringstream cin("5 5\n"
+                         "5 4 2 3 5\n"
+                         "2 0 3\n"
+                         "1 1 1\n"
+                         "2 0 3\n"
+                         "1 3 1\n"
+                         "2 0 5"s);
+  std::ostringstream cout;
+  std::string expected_cout("11\n"
+                            "8\n"
+                            "14\n");
+  Solution(cin, cout);
+  const auto actual_cout = cout.str();
+  assert(actual_cout == expected_cout);
+  std::cerr << "TestSolution OK" << std::endl;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+// #ifndef NDEBUG
+//   TestSolution();
+// #endif
+  Solution(std::cin, std::cout);
+  return 0;
+}
