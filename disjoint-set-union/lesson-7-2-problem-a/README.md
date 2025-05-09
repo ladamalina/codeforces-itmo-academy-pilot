9b3fb12f25271b33857f4b1d82bceb04d63b1e1f
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-05-09 13:55:03 +0300

    A. Люди уходят

---
+++ disjoint-set-union/lesson-7-2-problem-a/README.md
@@ -0,0 +1,3 @@
+# A. Люди уходят
+
+https://codeforces.com/edu/course/2/lesson/7/2/practice/contest/289391/problem/A
---
+++ disjoint-set-union/lesson-7-2-problem-a/main.cpp
@@ -0,0 +1,141 @@
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cfloat>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numbers>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvvi [[maybe_unused]] = std::vector<vvi>;
+using vvl [[maybe_unused]] = std::vector<vl>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+class DSU { // со сжатием путей, без ранговой эвристики
+public:
+  explicit DSU(int n) : p_(vi(n)) {
+    std::iota(p_.begin(), p_.end(), 0);
+  }
+
+  int Get(int a) {
+    return p_[a] = (a == p_[a] ? a : Get(p_[a]));
+  }
+
+  void Union(int a, int b) {
+    a = Get(a);
+    b = Get(b);
+    if (a == b) return;
+    p_[a] = b;
+  }
+
+private:
+  vi p_;
+};
+
+void Solution([[maybe_unused]] std::istream& cin, [[maybe_unused]] std::ostream& cout) {
+  int n, m; cin >> n >> m;
+  DSU dsu(n+2);
+
+  FOR(qi, 1, m) {
+    char op; cin >> op;
+    int x; cin >> x;
+    if (op == '-') {
+      dsu.Union(x, x + 1);
+    } else { // ?
+      const auto res = dsu.Get(x);
+      cout << (res <= n ? res : -1) << '\n';
+    }
+  }
+}
+
+#ifndef NDEBUG
+[[maybe_unused]] std::stringstream ReadFile(const std::string& path) {
+  std::ifstream file(path);
+  assert(file);
+  std::stringstream ss;
+  ss << file.rdbuf();
+  file.close();
+  return ss;
+}
+
+[[maybe_unused]] void RunTest(const std::string& in_path, const std::string& out_path) {
+  std::stringstream cin = ReadFile(in_path);
+  std::stringstream cout;
+  const auto e_out = ReadFile(out_path).str();
+  const auto start_t = std::chrono::high_resolution_clock::now();
+  Solution(cin, cout);
+  const auto end_t = std::chrono::high_resolution_clock::now();
+  const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+  std::cerr << in_path << ", "sv << total_t << " ms"sv << std::endl;
+  const auto a_out = cout.str();
+  assert(a_out == e_out);
+}
+
+[[maybe_unused]] void TestSolution() {
+  const std::string dir("../"s);
+  RunTest(dir + "input1.txt"s, dir + "output1.txt"s);
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+#endif
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+// #ifndef NDEBUG
+//   TestSolution();
+// #endif
+  Solution(std::cin, std::cout);
+  return 0;
+}
