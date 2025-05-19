6e3782e1972512cce188e25ba8198b3b2d5bf217
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-05-19 09:40:03 +0300

    C. Вложенные отрезки

---
+++ segment-tree-1/lesson-4-3-problem-c/README.md
@@ -0,0 +1,3 @@
+# C. Вложенные отрезки
+
+https://codeforces.com/edu/course/2/lesson/4/3/practice/contest/274545/problem/C
---
+++ segment-tree-1/lesson-4-3-problem-c/main.cpp
@@ -0,0 +1,138 @@
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define F first
+#define S second
+#define PB push_back
+
+template<typename T>
+class SegTree { // Дерево отрезков на сумму
+public:
+  explicit SegTree(const std::vector<T>& a) {
+    while (size_ < CI(a.size())) size_ *= 2;
+    t_.resize(size_ * 2 - 1);
+    Init(a, 0, 0, size_);
+  }
+
+  T Sum(int l, int r) {
+    return Sum(l, r, 0, 0, size_);
+  }
+
+  void Set(int i, T v) {
+    Set(i, v, 0, 0, size_);
+  }
+
+private:
+  std::vector<T> t_;
+  int size_ = 1;
+
+  void Init(const std::vector<T>& a, int x, int lx, int rx) {
+    if (lx + 1 == rx) {
+      if (lx < CI(a.size())) t_[x] = a[lx];
+    } else {
+      const auto m = (lx + rx) / 2;
+      Init(a, 2*x+1, lx, m);
+      Init(a, 2*x+2, m, rx);
+      t_[x] = t_[2*x+1] + t_[2*x+2];
+    }
+  }
+
+  T Sum(int l, int r, int x, int lx, int rx) {
+    if (rx <= l || lx >= r) return 0;
+    if (rx <= r && lx >= l) return t_[x];
+    const auto m = (lx + rx) / 2;
+    return Sum(l, r, 2*x+1, lx, m) + Sum(l, r, 2*x+2, m, rx);
+  }
+
+  void Set(int i, T v, int x, int lx, int rx) {
+    if (lx + 1 == rx) {
+      t_[x] = v;
+      return;
+    }
+    const auto m = (lx + rx) / 2;
+    if (i < m) Set(i, v, 2*x+1, lx, m);
+    else Set(i, v, 2*x+2, m, rx);
+    t_[x] = t_[2*x+1] + t_[2*x+2];
+  }
+};
+
+void Solution([[maybe_unused]] std::istream& cin, [[maybe_unused]] std::ostream& cout) {
+  int n; cin >> n;
+  vi a(n*2);
+  FOR(i, 0, n*2-1) cin >> a[i];
+  SegTree tree(vi(a.size()));
+  vi beg(n+1, -1);
+  vi res(n+1);
+  FOR(i, 0, 2*n-1) {
+    if (beg[a[i]] != -1) { // end
+      res[a[i]] = tree.Sum(beg[a[i]], i+1);
+      tree.Set(beg[a[i]], 1);
+    } else { // begin
+      beg[a[i]] = i;
+    }
+  }
+  FOR(i, 1, n) cout << res[i] << ' ';
+  cout << '\n';
+}
+
+#ifndef NDEBUG
+[[maybe_unused]] std::stringstream ReadFile(const std::string& path) {
+  std::ifstream file(path);
+  assert(file);
+  std::stringstream ss;
+  ss << file.rdbuf();
+  file.close();
+  return ss;
+}
+
+[[maybe_unused]] void RunTest(const std::string& in_path, const std::string& out_path) {
+  std::stringstream cin = ReadFile(in_path);
+  std::stringstream cout;
+  const auto e_out = ReadFile(out_path).str();
+  const auto start_t = std::chrono::high_resolution_clock::now();
+  Solution(cin, cout);
+  const auto end_t = std::chrono::high_resolution_clock::now();
+  const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+  std::cerr << in_path << ", "sv << total_t << " ms"sv << std::endl;
+  const auto a_out = cout.str();
+  assert(a_out == e_out);
+}
+
+[[maybe_unused]] void TestSolution() {
+  const std::string dir("../"s);
+  RunTest(dir + "input1.txt"s, dir + "output1.txt"s);
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+#endif
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+// #ifndef NDEBUG
+//   TestSolution();
+// #endif
+  Solution(std::cin, std::cout);
+  return 0;
+}
