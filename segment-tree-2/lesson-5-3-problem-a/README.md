0ca63ec0552ecd2b569fe3cd9df916e0e16b779e
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-05-24 11:39:28 +0300

    A. Присваивание и максимальный отрезок

---
+++ segment-tree-2/lesson-5-3-problem-a/README.md
@@ -0,0 +1,3 @@
+# A. Присваивание и максимальный отрезок
+
+https://codeforces.com/edu/course/2/lesson/5/3/practice/contest/280799/problem/A
---
+++ segment-tree-2/lesson-5-3-problem-a/main.cpp
@@ -0,0 +1,178 @@
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define F first
+#define S second
+#define PB push_back
+
+template<typename T>
+class SegTree { // Присваивание и максимальный отрезок
+public:
+  struct Node {
+    int cnt = 0;
+    T sum=0, p_sum=0, s_sum=0, max_sum=0;
+    T set = NO_OP;
+  };
+  
+  explicit SegTree(const std::vector<T>& a) {
+    while (size_ < CI(a.size())) size_ *= 2;
+    t_.resize(size_ * 2 - 1);
+    Init(a, 0, 0, size_);
+  }
+
+  void Modify(int l, int r, T v) {
+    Modify(l, r, v, 0, 0, size_);
+  }
+
+  T Get(int l, int r) {
+    return Get(l, r, 0, 0, size_).max_sum;
+  }
+
+private:
+  std::vector<Node> t_;
+  int size_ = 1;
+  static const T NO_OP = std::numeric_limits<T>::min();
+
+  static Node SetOp(const Node& x, T v) {
+    if (v == NO_OP) return x;
+    return {
+      x.cnt,
+      x.cnt * v,
+      x.cnt * (v>=0 ? v : 0),
+      x.cnt * (v>=0 ? v : 0),
+      x.cnt * (v>=0 ? v : 0),
+      v
+    };
+  }
+
+  static Node Combine(const Node& x, const Node& l, const Node& r) {
+    Node m{
+      l.cnt + r.cnt,
+      l.sum + r.sum,
+      std::max(l.p_sum, l.sum+r.p_sum),
+      std::max(r.s_sum, l.s_sum+r.sum),
+      std::max(l.s_sum+r.p_sum, std::max(l.max_sum, r.max_sum)),
+      x.set
+    };
+    return SetOp(m, x.set);
+  }
+
+  void Init(const std::vector<T>& a, int x, int lx, int rx) {
+    if (lx + 1 == rx) {
+      if (lx < CI(a.size())) {
+        t_[x].cnt = 1;
+        t_[x] = SetOp(t_[x], a[lx]);
+      }
+    } else {
+      const auto m = (lx + rx) / 2;
+      Init(a, 2*x+1, lx, m);
+      Init(a, 2*x+2, m, rx);
+      t_[x] = Combine(t_[x], t_[2*x+1], t_[2*x+2]);
+    }
+  }
+
+  void Propagate(int x, int lx, int rx) {
+    if (t_[x].set == NO_OP || lx + 1 == rx) return;
+
+    t_[2*x+1] = SetOp(t_[2*x+1], t_[x].set);
+    t_[2*x+2] = SetOp(t_[2*x+2], t_[x].set);
+    
+    t_[x].set = NO_OP;
+  }
+
+  void Modify(int l, int r, T v, int x, int lx, int rx) {
+    Propagate(x, lx, rx);
+    if (rx <= l || lx >= r) return;
+    if (rx <= r && lx >= l) {
+      t_[x] = SetOp(t_[x], v);
+      return;
+    }
+    const auto m = (lx + rx) / 2;
+    Modify(l, r, v, 2*x+1, lx, m);
+    Modify(l, r, v, 2*x+2, m, rx);
+    t_[x] = Combine(t_[x], t_[2*x+1], t_[2*x+2]);
+  }
+
+  Node Get(int l, int r, int x, int lx, int rx) {
+    if (rx <= l || lx >= r) return {};
+    if (rx <= r && lx >= l) {
+      return t_[x];
+    }
+    const auto m = (lx + rx) / 2;
+    const auto m1 = Get(l, r, 2*x+1, lx, m);
+    const auto m2 = Get(l, r, 2*x+2, m, rx);
+    return Combine(t_[x], m1, m2);
+  }
+};
+
+void Solution(std::istream& cin, std::ostream& cout) {
+  int n, m; cin >> n >> m;
+  SegTree tree(vl(n, 0));
+  FOR(j, 0, m-1) {
+    int l, r, v; cin >> l >> r >> v;
+    tree.Modify(l, r, v);
+    cout << tree.Get(0, n) << '\n';
+  }
+}
+
+#ifndef NDEBUG
+[[maybe_unused]] std::stringstream ReadFile(const std::string& path) {
+  std::ifstream file(path);
+  assert(file);
+  std::stringstream ss;
+  ss << file.rdbuf();
+  file.close();
+  return ss;
+}
+
+[[maybe_unused]] void RunTest(const std::string& in_path, const std::string& out_path) {
+  std::stringstream cin = ReadFile(in_path);
+  std::stringstream cout;
+  const auto e_out = ReadFile(out_path).str();
+  const auto start_t = std::chrono::high_resolution_clock::now();
+  Solution(cin, cout);
+  const auto end_t = std::chrono::high_resolution_clock::now();
+  const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+  std::cerr << in_path << ", "sv << total_t << " ms"sv << std::endl;
+  const auto a_out = cout.str();
+  assert(a_out == e_out);
+}
+
+[[maybe_unused]] void TestSolution() {
+  const std::string dir("../"s);
+  RunTest(dir + "input1.txt"s, dir + "output1.txt"s);
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+#endif
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+// #ifndef NDEBUG
+//   TestSolution();
+// #endif
+  Solution(std::cin, std::cout);
+  return 0;
+}
