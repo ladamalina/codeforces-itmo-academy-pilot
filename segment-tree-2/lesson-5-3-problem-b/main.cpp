a1295cb2a2adb84e0b029d7a83a33b42cf5a4c36
Nadezhda R <nadezhdaryabtsova@gmail.com>
2025-08-26 03:09:53 +0300

    B. Изменение и K-я единица

--- segment-tree-2/lesson-5-3-problem-b/main.cpp
+++ segment-tree-2/lesson-5-3-problem-b/main.cpp
@@ -1,30 +1,34 @@
-// #pragma GCC optimize("O3")
-// #pragma GCC optimize("unroll-loops")
-
+// #pragma GCC optimize("O3,unroll-loops")
 #include <bits/stdc++.h>
 
 using namespace std::literals;
 
 using ll = long long;
-using ii [[maybe_unused]] = std::pair<int, int>;
-using vi [[maybe_unused]] = std::vector<int>;
-using vl [[maybe_unused]] = std::vector<ll>;
-using vvi [[maybe_unused]] = std::vector<vi>;
-using vii [[maybe_unused]] = std::vector<ii>;
-using vb [[maybe_unused]] = std::vector<bool>;
-using vd [[maybe_unused]] = std::vector<double>;
-using vs [[maybe_unused]] = std::vector<std::string>;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
 
-#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
-#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
 #define RNG(_l) (_l).begin(), (_l).end()
 #define SORT(_l) std::sort((_l).begin(), (_l).end())
 #define CI(_v) static_cast<int>(_v)
 #define CL(_v) static_cast<ll>(_v)
-#define CD(_v) static_cast<double>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
 #define F first
 #define S second
-#define PB push_back
 
 template<typename T>
 class SegTree { // Изменение и K-я единица
@@ -36,17 +40,17 @@ public:
   };
   
   explicit SegTree(const std::vector<T>& a) {
-    while (size_ < CI(a.size())) size_ *= 2;
-    t_.resize(size_ * 2 - 1);
-    Init(a, 0, 0, size_);
+    while (size_ < SZ(a)) size_ <<= 1;
+    t_.resize(size_ << 1);
+    Init(a, 1, 0, size_);
   }
 
   void Modify(int l, int r) {
-    Modify(l, r, 0, 0, size_);
+    Modify(l, r, 1, 0, size_);
   }
 
   int Get(int i) {
-    return Get(i+1, 0, 0, size_);
+    return Get(i+1, 1, 0, size_);
   }
 
 private:
@@ -63,23 +67,23 @@ private:
 
   void Init(const std::vector<T>& a, int x, int lx, int rx) {
     if (lx + 1 == rx) {
-      if (lx < CI(a.size())) {
+      if (lx < SZ(a)) {
         t_[x].cnt = 1;
       }
     } else {
-      const auto m = (lx + rx) / 2;
-      Init(a, 2*x+1, lx, m);
-      Init(a, 2*x+2, m, rx);
-      t_[x] = Combine(t_[x], t_[2*x+1], t_[2*x+2]);
+      const auto m = (lx + rx) >> 1;
+      Init(a, (x<<1), lx, m);
+      Init(a, (x<<1)|1, m, rx);
+      t_[x] = Combine(t_[x], t_[(x<<1)], t_[(x<<1)|1]);
     }
   }
 
   void Propagate(int x, int lx, int rx) {
     if (!t_[x].rev || lx+1 == rx) return;
 
-    t_[2*x+1] = {t_[2*x+1].cnt, t_[2*x+1].cnt - t_[2*x+1].sum, !t_[2*x+1].rev};
-    t_[2*x+2] = {t_[2*x+2].cnt, t_[2*x+2].cnt - t_[2*x+2].sum, !t_[2*x+2].rev};
-    t_[x] = {t_[x].cnt, t_[2*x+1].sum + t_[2*x+2].sum, !t_[x].rev};
+    t_[(x<<1)] = {t_[(x<<1)].cnt, t_[(x<<1)].cnt - t_[(x<<1)].sum, !t_[(x<<1)].rev};
+    t_[(x<<1)|1] = {t_[(x<<1)|1].cnt, t_[(x<<1)|1].cnt - t_[(x<<1)|1].sum, !t_[(x<<1)|1].rev};
+    t_[x] = {t_[x].cnt, t_[(x<<1)].sum + t_[(x<<1)|1].sum, !t_[x].rev};
   }
 
   void Modify(int l, int r, int x, int lx, int rx) {
@@ -89,19 +93,19 @@ private:
       t_[x] = {t_[x].cnt, t_[x].cnt-t_[x].sum, !t_[x].rev};
       return;
     }
-    const auto m = (lx + rx) / 2;
-    Modify(l, r, 2*x+1, lx, m);
-    Modify(l, r, 2*x+2, m, rx);
-    t_[x] = Combine(t_[x], t_[2*x+1], t_[2*x+2]);
+    const auto m = (lx + rx) >> 1;
+    Modify(l, r, (x<<1), lx, m);
+    Modify(l, r, (x<<1)|1, m, rx);
+    t_[x] = Combine(t_[x], t_[(x<<1)], t_[(x<<1)|1]);
   }
 
   int Get(int i, int x, int lx, int rx) {
     Propagate(x, lx, rx);
     if (lx+1 == rx) return lx;
-    const auto m = (lx + rx) / 2;
-    if (t_[2*x+1].sum >= i)
-      return Get(i, 2*x+1, lx, m);
-    return Get(i-t_[2*x+1].sum, 2*x+2, m, rx);
+    const auto m = (lx + rx) >> 1;
+    if (t_[(x<<1)].sum >= i)
+      return Get(i, (x<<1), lx, m);
+    return Get(i-t_[(x<<1)].sum, (x<<1)|1, m, rx);
   }
 };
 
@@ -154,9 +158,9 @@ int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
-// #ifndef NDEBUG
-//   TestSolution();
-// #endif
+#ifndef NDEBUG
+  TestSolution();
+#endif
   Solution(std::cin, std::cout);
   return 0;
 }
